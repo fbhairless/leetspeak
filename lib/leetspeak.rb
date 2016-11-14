1d4312f89e41a25f309ab17873ac70a911fae18f
Hal Fairless and Jonathan Thom <hal+jonathan@>
2016-11-14 13:52:21 -0800

    It will not replace first s of a word

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -2,6 +2,10 @@ class String
   define_method(:leetspeak) do
     letters = self.split("")
     fixed_letters = []
+      if letters[0].eql?("s")
+        fixed_letters.push("s")
+        letters.shift
+      end
     letters.each() do |letter|
       if letter.eql?("e")
         fixed_letters.push("3")
@@ -9,7 +13,9 @@ class String
         fixed_letters.push("0")
       elsif letter.eql?("I")
         fixed_letters.push("1")
-      elsif letter.eql?("s") && letter != letters[0]
+      # elsif letter.eql?("s") && it is at index 0
+      #   fixed_letters.push("s")
+    elsif letter.eql?("s")
         fixed_letters.push("z")
       else
         fixed_letters.push(letter)
