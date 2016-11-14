a8e0592b4f972ca39d2e039a299ca6e7bb0d71a9
Hal Fairless and Jonathan Thom <jonathan@>
2016-11-14 14:05:16 -0800

    It will work for multiple word strings

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,26 +1,30 @@
 class String
   define_method(:leetspeak) do
-    letters = self.split("")
-    fixed_letters = []
-      if letters[0].eql?("s")
-        fixed_letters.push("s")
-        letters.shift
+    words = self.split(" ")
+    fixed_string = []
+    words.each() do |word|
+      letters = word.split("")
+      fixed_letters = []
+        if letters[0].eql?("s")
+          fixed_letters.push("s")
+          letters.shift
+        end
+      letters.each() do |letter|
+        if letter.eql?("e")
+          fixed_letters.push("3")
+        elsif letter.eql?("o")
+          fixed_letters.push("0")
+        elsif letter.eql?("I")
+          fixed_letters.push("1")
+        elsif letter.eql?("s")
+          fixed_letters.push("z")
+        else
+          fixed_letters.push(letter)
+        end
       end
-    letters.each() do |letter|
-      if letter.eql?("e")
-        fixed_letters.push("3")
-      elsif letter.eql?("o")
-        fixed_letters.push("0")
-      elsif letter.eql?("I")
-        fixed_letters.push("1")
-      # elsif letter.eql?("s") && it is at index 0
-      #   fixed_letters.push("s")
-    elsif letter.eql?("s")
-        fixed_letters.push("z")
-      else
-        fixed_letters.push(letter)
-      end
+      new_words = fixed_letters.join("")
+      fixed_string.push(new_words)
     end
-    fixed_letters.join("")
+    fixed_string.join(" ")
   end
 end
